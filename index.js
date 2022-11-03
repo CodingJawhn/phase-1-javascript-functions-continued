fca846e55b2fbfb74c74ee8b7d62358b85d9338a
John Yang <john.yang8097@gmail.com>
2022-11-02 21:56:57 -0400

    commit tree 62ebcac483232126ab80ec65be2ebe6261a8b4b1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity) {
+if (typeof activity !== `undefined`) {
+    return `This Saturday, I want to ${activity}!`
+}
+else
+return `This Saturday, I want to roller-skate!`
+}
+// when determining a value that is undefined, use typeof
+
+function mondayWork(activity) {
+    if (typeof activity !== `undefined`) {
+        return `This Monday, I will ${activity}.`
+    }
+    else
+    return `This Monday, I will go to the office.`
+}
+
+function wrapAdjective(result, msg = `You are`) {
+    const innerFunction = function (emphatic){
+        return `${msg} ${result}${emphatic}${result}!`
+    }
+    return innerFunction
+}
